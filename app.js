d6433a03ac6d797adcc6c309d06e3f439bad349b
Kaustav iDas <49059670+official-idas@users.noreply.github.com>
2020-01-10 08:51:50 +0530

    V5
    Manually Cors Written in App.js & Install body-Parser

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
+// Middleware
+// Morgan Use For to See App Log (Means App Calling Stats) in Terminal
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//CORS SETUP 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, PATCH, DELETE');
+      return res.status(200).json({});
+    }
+    next();
+})
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
-
-// Morgan Use For to See App Log (Means App Calling Stats) in Terminal
-app.use(morgan('dev'));
 
 
 // Routes Which Should handle Request
