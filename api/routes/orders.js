96605af25fa39b5b5d18eb18fd20d24f93ef3d23
Kaustav iDas <49059670+official-idas@users.noreply.github.com>
2020-01-14 10:47:38 +0530

    V8
    Mongoose Validate With Order APi Call and Check Product Id Match with Database

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,131 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders Get Request'
-    })
+    Order.find()
+         .exec()
+         .then(docs => {
+             console.log(docs);
+             const response = {
+               count : docs.length,
+               oders : docs.map(doc => {
+                 return {
+                    _id : doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + doc._id
+                    }
+                 }
+               })
+             }
+            if(docs.length >= 0)
+              res.status(201).json(response)
+            res.status(404).json({
+                message: 'No Data'
+            })  
+         })
+         .catch(err => {
+            res.status(500).json({
+                error : err
+            })
+         })
 })
 
+
+
 router.post('/', (req, res, next) => {
-    const order = {
-        productId : req.body.productId,
-        quantity : req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Orders Post Request',
-        order : order
-    })
+    //Checking Product Id is Matching With Order Product Id
+    Product.findById({ _id : req.body.productId })
+           .then(product => {
+               //Order Post Api Start
+                if(!product) {
+                   res.status(404).json({
+                       message: 'Product Not Found'
+                   }) 
+                }
+                const order = new Order({
+                    _id : mongoose.Types.ObjectId(), 
+                    product: req.body.productId,
+                    quantity: req.body.quantity
+                });
+            
+                return order.save()  
+                //  Order Post Api End             
+           })
+           .then(result => {
+            console.log(result); 
+            res.status(201).json({
+                message: 'Orders Post Request',
+                createdOrder: {
+                    _id : result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' + result._id
+                }
+            })
+            // res.status(201).json(result)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error : err
+            })
+        })
+        //    .catch(err => {
+        //        res.status(500).json({
+        //            message: 'Product Not Found',
+        //            error : err
+        //        })
+        //    })
+
 })
 
 router.get('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
-        res.status(200).json({
-            message: 'Single order Found',
-            id: id
-        })
+
+    Order.findById({ _id : id})
+         .exec()
+         .then(doc => {
+           if (doc) 
+           res.status(201).json({
+            _id : doc._id,
+            product : doc.product,
+            quantity : doc.quantity
+           })
+           res.status(404).json({
+               message : 'Data Not In Database'
+           })
+         })
+         .catch(err => {
+             res.status(500).json({
+                error : err 
+             })
+         })
 })
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Order'
-    })
+  const deleteId = req.params.orderId;
+  Order.deleteOne({ _id : deleteId })
+       .exec()
+       .then(result => {
+           res.status(201).json({
+               message : 'Order Id Deleted : ' + deleteId
+           })
+       })
+       .catch(err => {
+           res.status(500).json({
+               error : err
+           })
+       })
 })
 
 
